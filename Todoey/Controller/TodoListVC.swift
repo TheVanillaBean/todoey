68361e88f64098d68f80a90217b26ab2ed77649e
Alex Alimov <teambluepin@gmail.com>
2018-02-21 03:23:11 -0700

    Persit data with Codable

--- Todoey/Controller/TodoListVC.swift
+++ Todoey/Controller/TodoListVC.swift
@@ -8,26 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController{
+class TodoListVC: UIViewController{
     
     @IBOutlet weak var tableView: UITableView!
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destory Demo"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
-        
+        loadItems()
     }
     
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding list, \(error)")
+            }
+        }
+    }
     
     @IBAction func addBtnPressed(_ sender: UIBarButtonItem) {
         
@@ -35,12 +52,13 @@ class ViewController: UIViewController{
         
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
             if let text = textField.text{
-                
-                self.itemArray.append(text)
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+                self.itemArray.append(Item(title: text))
+                self.saveItems()
                 self.tableView.reloadData()
             }
+            
         }
         
         alert.addTextField { (alertTextField) in
@@ -53,27 +71,25 @@ class ViewController: UIViewController{
     
 }
 
-extension ViewController: UITableViewDataSource, UITableViewDelegate {
+extension TodoListVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = itemArray[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.isDone ? .checkmark : .none
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        
+        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+        self.saveItems()
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
